06bb8da34fb8b89479709670b9d95c5248047c07
Linco Roffimas <lrnzdiogo@gmail.com>
2024-08-19 16:05:47 +0000

    first_commit

--- insert_data.sh
+++ insert_data.sh
@@ -9,60 +9,46 @@ fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
+# Clear existing data
 echo $($PSQL "TRUNCATE games, teams")
 
-cat games.csv | while IFS="," read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+# Read games.csv
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  if [[ $YEAR != "year" && $ROUND != "round" && $WINNER != "winner" && $OPPONENT != "opponent" && $WINNER_GOALS != "winner_goals" && $OPPONENT_GOALS != "opponent_goals" ]]
+  # Skip the header row
+  if [[ $YEAR != "year" ]]
   then
-    # Get winner team_id
+    # Get the team_id from winner and opponent
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    
-    # If not found
-    if [[ -z $WINNER_ID ]]
-    then
-      # Insert team
-      INSERT_WINNER=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
-
-      # If inserted success
-      if [[ $INSERT_WINNER == "INSERT 0 1" ]]
-      then
-        echo $INSERT_WINNER
-
-        # Get new winner team_id
-        WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-        WINNER_NAME=$($PSQL "SELECT name FROM teams WHERE team_id='$WINNER_ID'")
-        echo "Insert success: ${WINNER_NAME}"
-      fi
-    fi
-
-    # Get opponent team_id
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
 
-    # If not found
-    if [[ -z $OPPONENT_ID ]]
+    # If winner not found, insert it into the teams table
+    if [[ -z $WINNER_ID ]]
     then
-      # Insert team
-      INSERT_OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-
-      # If inserted success
-      if [[ $INSERT_OPPONENT == "INSERT 0 1" ]]
+      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
       then
-        echo $INSERT_OPPONENT
-
-        # Get new opponent team_id
-        OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-        OPPONENT_NAME=$($PSQL "SELECT name FROM teams WHERE team_id='$OPPONENT_ID'")
-        echo "Inserted success: ${OPPONENT_NAME}"
+        echo "Inserted into teams: $WINNER"
       fi
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     fi
 
-    # Insert Games
-    INSERT_GAMES=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
-    if [[ $INSERT_GAMES == "INSERT 0 1" ]]
+    # If opponent not found, insert it into the teams table
+    if [[ -z $OPPONENT_ID ]]
     then
-      echo $INSERT_GAMES
-      echo "Insert success: $YEAR, $ROUND, $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS"
+      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted into teams: $OPPONENT"
+      fi
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+
+    # Insert the game data into the games table
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+    then
+      echo "Inserted into games: $YEAR $ROUND - $WINNER vs $OPPONENT"
     fi
   fi
 done
--- queries.sh
+++ queries.sh
@@ -26,16 +26,16 @@ echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo "$($PSQL "SELECT name FROM teams JOIN games ON teams.team_id = games.winner_id WHERE year = 2018 AND round = 'Final'")"
+echo "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id=games.winner_id WHERE year=2018 AND round='Final'")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "$($PSQL "SELECT DISTINCT name FROM teams JOIN games ON teams.team_id IN (games.winner_id, games.opponent_id) WHERE year = 2014 AND round = 'Eighth-Final' ORDER BY name")"
+echo "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id=games.winner_id OR teams.team_id=games.opponent_id WHERE year=2014 AND round='Eighth-Final' ORDER BY name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo "$($PSQL "SELECT DISTINCT name FROM teams JOIN games ON teams.team_id = games.winner_id ORDER BY name")"
+echo "$($PSQL "SELECT DISTINCT(name) FROM teams INNER JOIN games ON teams.team_id=games.winner_id ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo "$($PSQL "SELECT year, name FROM teams JOIN games ON teams.team_id = games.winner_id WHERE round = 'Final' ORDER BY year")"
+echo "$($PSQL "SELECT year, name FROM teams INNER JOIN games ON teams.team_id=games.winner_id WHERE round='Final' ORDER BY year")"
 
 echo -e "\nList of teams that start with 'Co':"
 echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%' ORDER BY name")"
